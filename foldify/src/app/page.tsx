9aa6ccd4796f2db8c5e403917586f60294abfce7
Chai Landau <clandau@octoml.ai>
2024-02-14 18:53:33 -0500

    feat: add foldify demo

---
+++ foldify/src/app/page.tsx
@@ -0,0 +1,15 @@
+import GeneratedImage from "@/components/GeneratedImage";
+
+export default function Home() {
+  return (
+    <main className="container">
+      <div className="hero">
+        <h1>
+          Create your own <span>beautiful origami piece</span>
+        </h1>
+      </div>
+
+      <GeneratedImage />
+    </main>
+  );
+}
