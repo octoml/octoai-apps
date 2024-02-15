6d7fae06471cb8222efdfb928d1586b52e698a2d
Chai Landau <clandau@octoml.ai>
2024-02-15 11:22:08 -0500

    feat: add comicverse

---
+++ comicverse/src/app/api/generate/route.ts
@@ -0,0 +1,45 @@
+import { Client } from "@octoai/client";
+import { NextRequest } from "next/server";
+
+// Pass OctoAI Token into the client for authorization
+const client = new Client(process.env.OCTOAI_TOKEN);
+
+export async function POST(request: NextRequest) {
+  try {
+    const data = await request.json();
+
+    const inferenceData = await client.inferAsync(
+      "https://image.octoai.run/generate/sdxl",
+      {
+        prompt: data.prompt,
+        negative_prompt: "",
+        style_preset: "comic-book",
+        sampler: "DDIM",
+        checkpoint: "octoai:lcm_sdxl",
+        steps: 30,
+        cfg_scale: 12,
+        width: 1024,
+        height: 1024,
+        use_refiner: true,
+        high_noise_frac: 0.8,
+      }
+    );
+
+    // Check if `inferenceData` is ready
+    let ready = await client.isFutureReady(inferenceData);
+
+    // If `inferenceData` is not ready, keep trying endpoint until it is
+    while (!ready) {
+      ready = await client.isFutureReady(inferenceData);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+
+    // Once `inferenceData` is resolved, send back to client
+    const inferenceOutput = await client.getFutureResult(inferenceData);
+
+    return Response.json({ ...inferenceOutput });
+  } catch (error) {
+    console.error(`Error generating image: ${error}`);
+    return Response.error();
+  }
+}
---
+++ comicverse/src/components/Footer.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <nav>
+          <ul>
+            <li className="with-icon">
+              <a
+                href="https://github.com/octoml/octoai-apps/tree/main/comicverse"
+                target="_blank"
+              >
+                <Image
+                  src="/icons/github.svg"
+                  alt="Github Icon"
+                  width="18"
+                  height="18"
+                />
+                View on Github
+              </a>
+            </li>
+            <li className="with-icon">
+              <a
+                href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Foctoml%2Foctoai-apps%2Ftree%2Fmain%2Fcomicverse&env=OCTOAI_TOKEN&envDescription=Create%20an%20OctoAI%20token%20if%20you%20don't%20already%20have%20one.&envLink=https%3A%2F%2Focto.ai%2Fdocs%2Fgetting-started%2Fhow-to-create-an-octoai-access-token&project-name=my-comicverse-app&repository-name=my-comicverse-app"
+                target="_blank"
+              >
+                <Image
+                  src="/icons/vercel.svg"
+                  alt="Vercel Icon"
+                  width="18"
+                  height="18"
+                />
+                Deploy to Vercel
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="https://octo.ai/?utm_source=comicverse" target="_blank">
+                Powered by OctoAI
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://octo.ai/legals/privacy-policy/?utm_source=comicverse"
+                target="_blank"
+              >
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://octo.ai/legals/terms-of-use/?utm_source=comicverse"
+                target="_blank"
+              >
+                Terms of Use
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
