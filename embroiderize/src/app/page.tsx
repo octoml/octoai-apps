a5fcd3cfc0c3a3178bab7f8e96dc1d8a1ff68682
Chai Landau <clandau@octoml.ai>
2024-02-16 11:43:03 -0500

    feat: add credit

--- embroiderize/src/app/page.tsx
+++ embroiderize/src/app/page.tsx
@@ -9,6 +9,19 @@ export default function Home() {
         </h1>
       </div>
       <GeneratedImage />
+      <p className="credit">
+        Embroiderize uses{" "}
+        <a
+          href="https://huggingface.co/ostris/embroidery_style_lora_sdxl"
+          target="_blank"
+        >
+          Embroidery Style SDXL LoRA
+        </a>{" "}
+        by{" "}
+        <a href="https://ostris.com/" target="_blank">
+          Jaret Burkett.
+        </a>
+      </p>
     </main>
   );
 }
